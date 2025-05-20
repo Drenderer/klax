2b4543a31842f1effc6405d401ec60381171ec54
jaosch <jasper.schommartz@gmail.com>
2025-05-20 10:55:00 +0200

    Include accidently deleted alias of `parmax.unwrap`

--- klax/__init__.py
+++ klax/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from paramax import unwrap as unwrap
+
 from ._callbacks import (
     Callback as Callback,
     CallbackArgs as CallbackArgs,
