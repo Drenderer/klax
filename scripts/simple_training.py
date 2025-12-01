97e3060a282a18f765ca75d6179e2a6ad837df33
Fabian Roth <roth@cps.tu-darmstadt.de>
2025-12-01 13:05:49 -0800

    Added simple benchmark script for measuring training time.

---
+++ scripts/simple_training.py
@@ -0,0 +1,24 @@
+from jax import numpy as jnp
+from jax import random as jr
+from matplotlib import pyplot as plt
+
+import klax
+
+key = jr.key(0)
+model_key, train_key = jr.split(key, 2)
+
+x = jnp.linspace(-1, 1, 100)
+y = jnp.sin(x)
+
+model = klax.nn.MLP("scalar", "scalar", [16], key=model_key)
+
+model, history = klax.fit(
+    model,
+    (x, y),
+    steps=10_000,
+    key=train_key,
+)
+
+history.plot()
+plt.legend
+plt.show()
