5b0d1c82e3f0fc5e5440bea97a98704857f9ab27
Fabian Roth <roth@cps.tu-darmstadt.de>
2026-01-27 17:32:52 +0100

    Updated the examples to the new generalized training loop api.

--- docs/examples/continued_training.py
+++ docs/examples/continued_training.py
@@ -19,14 +19,11 @@ It shows how to initialize the optax initializer state with a previous training 
 and how to use the `HistoryCallback` to add the training history to the subsequent training sessions.
 """
 
-from functools import partial
-
 import jax.random as jr
 import optax
 from matplotlib import pyplot as plt
 
 import klax
-from klax import HistoryCallback
 
 key = jr.key(0)
 data_key, model_key, train1_key, train2_key = jr.split(key, 4)
@@ -37,14 +34,20 @@ y = 2 * x.sum(axis=-1) + 1.0
 y += 0.01 * jr.normal(data_key, y.shape)  # Add some noise
 
 
-def history_factory():
-    return HistoryCallback(
-        metric_defs={
-            "training_loss": ((x, y), partial(klax.mse, batch_axes=0)),
-        },
-        log_every=10,
-        verbose=True,
+def logger_factory():
+    logger = klax.MetricLogger(log_every=10)
+    logger.add_metric(
+        "training_loss",
+        klax.LossMetric(
+            klax.batch_data,
+            (x, y),
+            batch_size=x.shape[0],
+            batch_axes=0,
+            loss=klax.mse,
+            key=data_key,
+        ),
     )
+    return logger
 
 
 # A: Complete training for 2000 steps
@@ -54,66 +57,65 @@ model, history_complete = klax.fit(
     (x, y),
     steps=2000,
     optimizer=optax.adabelief(1e-5),
-    history=history_factory(),
+    logger=logger_factory(),
     key=train1_key,
 )
 
-# B: Training split into two sessions with 1000 steps each.
+# B: Training split into two sessions with 100 and 1900 steps, respectively.
 #    The optimizer state of the second session is initialized
 #    with the last optimizer state from the first session.
+#    We still expect slight differences from the continuous training,
+#    since we get different batches in the second session.
 model = klax.nn.MLP(2, "scalar", 2 * [16], key=model_key)
 model, history_continued = klax.fit(
     model,
     (x, y),
-    steps=1000,
+    steps=100,
     optimizer=optax.adabelief(1e-5),
-    history=history_factory(),
+    logger=logger_factory(),
     key=train1_key,
 )
 
-model, history_continued = klax.fit(
+model, history_continued_2 = klax.fit(
     model,
     (x, y),
-    steps=1000,
+    steps=1900,
     optimizer=optax.adabelief(1e-5),  # (!) Same optimizer as in first session
-    init_opt_state=history_continued.last_opt_state,  # Initialize the optimizer state with the last state
-    history=history_continued,  # Continue the history
+    init_opt_state=history_continued.final_opt_state,  # Initialize the optimizer state with the last state
+    logger=logger_factory(),
     key=train2_key,
 )
-
+history_continued.extend(history_continued_2)
 
 # C: Training split into two sessions with reset optimizer state.
 model = klax.nn.MLP(2, "scalar", 2 * [16], key=model_key)
 model, history_reset = klax.fit(
     model,
     (x, y),
-    steps=1000,
+    steps=100,
     optimizer=optax.adabelief(1e-5),
-    history=history_factory(),
+    logger=logger_factory(),
     key=train1_key,
 )
 
-model, history_reset = klax.fit(
+model, history_reset_2 = klax.fit(
     model,
     (x, y),
-    steps=1000,
+    steps=1900,
     optimizer=optax.adabelief(1e-5),  # (!) Same optimizer as in first session
     init_opt_state=None,  # No optimizer state is provided, so the optimizer is again initialized from scratch
-    history=history_reset,  # Continue the history
+    logger=logger_factory(),
     key=train2_key,
 )
+history_reset.extend(history_reset_2)
 
-
+# D: Plot the recorded losses
 fig, ax = plt.subplots()
 for histroy, label in zip(
     [history_complete, history_continued, history_reset],
     ["Continuous training", "Continued training", "Reset optimizer state"],
 ):
-    ax.plot(
-        history_complete.steps,
-        history_complete.metrics["training_loss"],
-        label=label,
-    )
+    histroy.plot("training_loss", ax=ax, label="Recorded loss - " + label)
 ax.set(
     title="Comparison of training loss histories",
     yscale="log",
